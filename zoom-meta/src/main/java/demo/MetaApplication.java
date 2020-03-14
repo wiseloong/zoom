ef5399baf1358211115cd167f744b34dd665d7a6
Anyhow-crane <582538746@qq.com>
2020-03-14 23:47:55 +0800

    事例项目修改，元对象项目

--- zoom-meta/src/main/java/demo/MetaApplication.java
+++ zoom-meta/src/main/java/demo/MetaApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication {
+@SpringBootApplication(scanBasePackages = {"demo", "com.zoom.meta"})
+public class MetaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MetaApplication.class, args);
     }
 
 }
--- zoom-meta/src/test/java/com/zoom/meta/MetaApplicationTests.java
+++ zoom-meta/src/test/java/com/zoom/meta/MetaApplicationTests.java
@@ -1,10 +1,11 @@
-package com.example.demo;
+package com.zoom.meta;
 
+import demo.MetaApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootTest(classes = MetaApplication.class)
+class MetaApplicationTests {
 
     @Test
     void contextLoads() {
