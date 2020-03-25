e2d736d7b1324c7f84834361668b03f3d0917d0f
Anyhow-crane <582538746@qq.com>
2020-03-25 17:09:10 +0800

    修改测试路径 防止项目之间相互依赖

--- zoom-security/src/main/java/demo/security/SecurityApplication.java
+++ zoom-security/src/main/java/demo/security/SecurityApplication.java
@@ -1,9 +1,9 @@
-package demo;
+package demo.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"demo", "com.zoom.security"})
+@SpringBootApplication(scanBasePackages = {"demo.security", "com.zoom.security"})
 public class SecurityApplication {
 
     public static void main(String[] args) {
