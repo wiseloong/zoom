4e1a48d31ca1e70b107865a8fa4c08b313eda2ef
Anyhow-crane <582538746@qq.com>
2020-05-26 09:27:30 +0800

    错误修复

--- zoom-flow/src/main/java/com/zoom/flow/FlowApplication.java
+++ zoom-flow/src/main/java/com/zoom/flow/FlowApplication.java
@@ -2,23 +2,12 @@ package com.zoom.flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class FlowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlowApplication.class, args);
     }
-
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.zoom"))
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
 
 }
